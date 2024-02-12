63db8869a000bdad69f4c42b1ba08bc0044e4ae3
melquiseded85 <melquiseded.serrano@implantait.com.br>
2024-02-12 15:49:19 -0300

    removido um botão

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,6 @@ namespace ProjetoTeste
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -76,22 +75,11 @@ namespace ProjetoTeste
             this.button3.Text = "tratamento";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(406, 346);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(236, 140);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "tratamento";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(873, 632);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -109,7 +97,6 @@ namespace ProjetoTeste
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
